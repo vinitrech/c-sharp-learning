83b0d2b576a831a799071fefc6166e33864ee325
Vinicius <vinitrech@gmail.com>
2023-03-25 17:03:11 -0300

    double question mark nullish coalescence

--- NullableTypes/Program.cs
+++ NullableTypes/Program.cs
@@ -9,6 +9,14 @@ namespace NullableTypes
             int? randNum = null;
             Console.WriteLine("randNum is equal to null: {0}", randNum == null);
             Console.WriteLine("randNum is equal to null: {0}", !randNum.HasValue);
+
+            int? testNull = null;
+            testNull ??= 5; // if testNull is null, assign 5
+
+            Console.WriteLine("testNull is equal to null: {0} with value {1}", testNull == null, testNull);
+
+            int? testNull2 = testNull ?? 10; // if testNull is null, assign 10. If is not null, assign testNull
+            Console.WriteLine("testNull2 is equal to null: {0} with value {1}", testNull2 == null, testNull2);
         }
     }
 }
