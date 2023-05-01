ba9d8466a4800005e650ac648a5b89be1cea5cdc
Vinicius <vinitrech@gmail.com>
2023-05-01 12:25:34 -0300

    adding records

---
+++ Records/Records.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Records
+{
+    public record Info(string FullName, int Age);
+    public record struct InfoAsStruct(string FullName, int Age); // struct record uses value-type parameters, the default is record class, which uses reference types
+
+    public record JobInfo
+    {
+        public string Name { get; init; } = default; // init means the value will be assigned on initialization only
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var info = new Info("Full name", 14);
+            var infoAsStruct = new InfoAsStruct("Full name 2", 52);
+            var jobInfo = new JobInfo("Job name");
+        }
+    }
+}
